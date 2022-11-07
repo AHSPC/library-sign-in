9b0f08540fb4c1b2f2434766c62164731a2e27ea
theMackabu <theMackabu@gmail.com>
2022-11-06 19:50:02 -0800

    reasons enum added

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -9,6 +9,28 @@ import (
 	nanoid "github.com/matoous/go-nanoid/v2"
 )
 
+const (
+	LUNCH int64 = iota
+	SENT_BY_TEACHER
+	FREE_PERIOD
+	BEFORE_OR_AFTER_SCHOOL
+)
+
+func Reason(level int64) string {
+	switch level {
+	case LUNCH:
+		return "lunch"
+	case SENT_BY_TEACHER:
+		return "sent by teacher"
+	case FREE_PERIOD:
+		return "free period"
+	case BEFORE_OR_AFTER_SCHOOL:
+		return "before/after school"
+	default:
+		return "reason unspecified"
+	}
+}
+
 func InsertStudent(db *sql.DB, name string, firstname string, lastname string, period int64, reason int64) error {
 	insertStudentSQL := `INSERT INTO %s(id, firstname, lastname, period, reason, date) VALUES (?, ?, ?, ?, ?, ?)`
 	statement, err := db.Prepare(fmt.Sprintf(insertStudentSQL, name))
@@ -17,7 +39,7 @@ func InsertStudent(db *sql.DB, name string, firstname string, lastname string, p
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	_, err = statement.Exec(id, firstname, lastname, period, reason, time.Now().Format(time.RFC3339))
+	_, err = statement.Exec(id, firstname, lastname, period, Reason(reason), time.Now().Format(time.RFC3339))
 
 	if err != nil {
 		log.Fatalln(err.Error())
