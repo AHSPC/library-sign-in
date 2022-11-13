1ca7f900f68cdbc2e39dec505eb3aa277e2baa8d
theMackabu <theMackabu@gmail.com>
2022-11-13 12:28:00 -0800

    cleanup theme selector

--- frontend/src/components/elements/Theme.tsx
+++ frontend/src/components/elements/Theme.tsx
@@ -6,81 +6,77 @@ import { classNames } from '@/helpers';
 import { useLocation } from 'react-router-dom';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 
-const LightIcon = (props: any) => {
-	return (
-		<svg viewBox='0 0 24 24' fill='none' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' {...props}>
-			<path
-				d='M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
-				css={localStorage.theme == 'light' ? tw`fill-sky-400/20 stroke-sky-500` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
-			<path
-				d='M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836'
-				css={localStorage.theme == 'light' ? tw`stroke-sky-500` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
-		</svg>
-	);
-};
-
-const DarkIcon = (props: any) => {
-	return (
-		<svg viewBox='0 0 24 24' fill='none' {...props}>
-			<path
-				fillRule='evenodd'
-				clipRule='evenodd'
-				d='M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z'
-				css={localStorage.theme == 'dark' ? tw`fill-sky-400/20` : tw`fill-transparent`}></path>
-			<path
-				d='m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z'
-				css={localStorage.theme == 'dark' ? tw`fill-sky-500` : tw`fill-slate-400 dark:fill-slate-500`}></path>
-			<path
-				fillRule='evenodd'
-				clipRule='evenodd'
-				d='M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z'
-				css={localStorage.theme == 'dark' ? tw`fill-sky-500` : tw`fill-slate-400 dark:fill-slate-500`}></path>
-		</svg>
-	);
-};
-
-const SystemIcon = (props: any) => {
-	return (
-		<svg viewBox='0 0 24 24' fill='none' {...props}>
-			<path
-				d='M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6Z'
-				strokeWidth='2'
-				strokeLinejoin='round'
-				css={!('theme' in localStorage) ? tw`fill-sky-400/20 stroke-sky-500` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
-			<path
-				d='M14 15c0 3 2 5 2 5H8s2-2 2-5'
-				strokeWidth='2'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				css={!('theme' in localStorage) ? tw`stroke-sky-500 fill-transparent` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
-		</svg>
-	);
-};
-
 const Theme = () => {
 	const location = useLocation();
+	const [theme, setTheme] = useState('');
 
-	const themes = [
-		{ name: 'Light', value: 'light', icon: LightIcon },
-		{ name: 'Dark', value: 'dark', icon: DarkIcon },
-		{ name: 'System', value: 'system', icon: SystemIcon },
-	];
+	const LightIcon = (props: any) => {
+		return (
+			<svg viewBox='0 0 24 24' fill='none' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' {...props}>
+				<path
+					d='M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
+					css={theme == 'light' ? tw`fill-sky-400/20 stroke-sky-500` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
+				<path
+					d='M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836'
+					css={theme == 'light' ? tw`stroke-sky-500` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
+			</svg>
+		);
+	};
+
+	const DarkIcon = (props: any) => {
+		return (
+			<svg viewBox='0 0 24 24' fill='none' {...props}>
+				<path
+					fillRule='evenodd'
+					clipRule='evenodd'
+					d='M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z'
+					css={theme == 'dark' && 'theme' in localStorage ? tw`fill-sky-400/20` : tw`fill-transparent`}></path>
+				<path
+					d='m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z'
+					css={theme == 'dark' && 'theme' in localStorage ? tw`fill-sky-500` : tw`fill-slate-400 dark:fill-slate-500`}></path>
+				<path
+					fillRule='evenodd'
+					clipRule='evenodd'
+					d='M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z'
+					css={theme == 'dark' && 'theme' in localStorage ? tw`fill-sky-500` : tw`fill-slate-400 dark:fill-slate-500`}></path>
+			</svg>
+		);
+	};
+
+	const SystemIcon = (props: any) => {
+		return (
+			<svg viewBox='0 0 24 24' fill='none' {...props}>
+				<path
+					d='M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6Z'
+					strokeWidth='2'
+					strokeLinejoin='round'
+					css={!('theme' in localStorage) ? tw`fill-sky-400/20 stroke-sky-500` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
+				<path
+					d='M14 15c0 3 2 5 2 5H8s2-2 2-5'
+					strokeWidth='2'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					css={!('theme' in localStorage) ? tw`stroke-sky-500 fill-transparent` : tw`stroke-slate-400 dark:stroke-slate-500`}></path>
+			</svg>
+		);
+	};
 
 	const updateTheme = () => {
 		if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
 			document.documentElement.classList.add('dark');
+			setTheme('dark');
 		} else {
 			document.documentElement.classList.remove('dark');
+			setTheme('light');
 		}
 	};
 
-	const setTheme = (mode: string) => {
-		localStorage.theme = mode;
-		updateTheme();
-	};
-
-	const setAuto = () => {
-		localStorage.removeItem('theme');
+	const updateStorage = (mode?: string, remove?: boolean) => {
+		if (remove) {
+			localStorage.removeItem('theme');
+		} else {
+			localStorage.theme = mode;
+		}
 		updateTheme();
 	};
 
@@ -91,8 +87,9 @@ const Theme = () => {
 	return (
 		<Menu as='div' tw='relative inline-block text-left'>
 			<div>
-				<Menu.Button tw='flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5'>
-					<ChevronDownIcon tw='m-1 h-6 w-6' aria-hidden='true' />
+				<Menu.Button tw='flex h-8 w-8 items-center justify-center rounded-lg shadow-sm shadow-black/5 ring-1 bg-white ring-black/5 dark:bg-slate-800 dark:ring-inset dark:ring-white/5'>
+					<LightIcon className='w-6 h-6 dark:hidden' />
+					<DarkIcon className='w-6 h-6 hidden dark:block' />
 				</Menu.Button>
 			</div>
 
@@ -109,13 +106,13 @@ const Theme = () => {
 						<Menu.Item>
 							{({ active }) => (
 								<a
-									onClick={() => setTheme('light')}
+									onClick={() => updateStorage('light')}
 									css={localStorage.theme == 'light' && tw`text-sky-600`}
 									className={classNames(
 										active ? 'bg-gray-50 dark:bg-slate-700' : '',
 										'block p-1 text-sm text-gray-800 dark:text-gray-300 my-0.5 cursor-pointer'
 									)}>
-									<LightIcon css={localStorage.theme == 'light' && tw`fill-sky-400 dark:fill-sky-400`} className='ml-1 inline h-6 w-6 mr-2 -mt-0.5' />
+									<LightIcon className='ml-1 inline h-6 w-6 mr-2 -mt-0.5' />
 									Light
 								</a>
 							)}
@@ -123,7 +120,7 @@ const Theme = () => {
 						<Menu.Item>
 							{({ active }) => (
 								<a
-									onClick={() => setTheme('dark')}
+									onClick={() => updateStorage('dark')}
 									css={localStorage.theme == 'dark' && tw`dark:text-sky-500`}
 									className={classNames(
 										active ? 'bg-gray-50 dark:bg-slate-700' : '',
@@ -137,13 +134,13 @@ const Theme = () => {
 						<Menu.Item>
 							{({ active }) => (
 								<a
-									onClick={() => setAuto()}
+									onClick={() => updateStorage('', true)}
 									css={!('theme' in localStorage) && tw`dark:text-sky-500`}
 									className={classNames(
 										active ? 'bg-gray-50 dark:bg-slate-700' : '',
 										'block p-1 text-sm text-gray-800 dark:text-gray-300 my-0.5 cursor-pointer'
 									)}>
-									<SystemIcon css={!('theme' in localStorage) && tw`fill-sky-400 dark:fill-sky-400`} className='ml-1 inline h-6 w-6 mr-2 -mt-0.5' />
+									<SystemIcon className='ml-1 inline h-6 w-6 mr-2 -mt-0.5' />
 									System
 								</a>
 							)}
