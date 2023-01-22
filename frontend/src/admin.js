f4900d1aedb6bf25d5072c42cbcf6449ebd7e949
blobbybilb <58201828+blobbybilb@users.noreply.github.com>
2023-01-21 19:15:03 -0800

    save

--- frontend/src/admin.js
+++ frontend/src/admin.js
@@ -1,10 +1,39 @@
-import { JsonToTable } from 'react-json-to-table'
+import { JsonToTable } from "react-json-to-table";
+import { useState } from "react";
 
 export default function Admin() {
-	const getData = () => {
-		// Sajjaad you write this bit please I give up
-		return {"placeholder": "lol"}
-	}
-	let data = getData()
-	return (<JsonToTable json={data}/>)
+  const [data, setData] = useState({});
+
+  const getData = async (username, password) =>
+    await (
+      await fetch("http://localhost:5010/api/v1/admin/list", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${
+            (
+              await (
+                await fetch("http://localhost:5010/api/v1/admin/login", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    username: username,
+                    password: password,
+                  }),
+                })
+              ).json()
+            ).token
+          }`,
+        },
+      })
+    ).json();
+
+  getData("admin", "admin").then((d) => {
+    setData(d);
+  });
+
+  // let data = { test: "test" };
+  // REACT SUCKS
+  return <JsonToTable json={data} />;
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App';
-import Confirm from './confirm';
-import Admin from './admin'
+import App from "./App";
+import Confirm from "./confirm";
+import Admin from "./admin";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<BrowserRouter>
-	<Routes>
-	<Route path="/"> 
-	<Route index element={<App device="iPad"/>}/> // Default Route
-	<Route path="qr" element={<App device="QR" />} /> // QR code route
-	<Route path="confirm" element={<Confirm />} /> // Confirm Screen route
-	<Route path="admin" element={<Admin />} />
-	</Route>
-	</Routes>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<App device="iPad" />} /> // Default Route
+        <Route path="qr" element={<App device="QR" />} /> // QR code route
+        <Route path="confirm" element={<Confirm />} /> // Confirm Screen route
+        <Route path="admin" element={<Admin />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
