dd14afb1929c6c132fb3fc40ca8203c4f3443bc9
FaunuX <judahpspiegel@gmail.com>
2023-01-25 17:40:53 -0800

    fixed infinite request bug

--- frontend/src/admin.js
+++ frontend/src/admin.js
@@ -1,14 +1,15 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
 import { JsonToTable } from 'react-json-to-table'
 
 export default function Admin() {
 	let [ tableData, setTableData ] = useState({})
 	const getData = async (username, password) => await ( await fetch("/api/v1/admin/list", { method: "GET", headers: { Authorization: `Bearer ${ ( await ( await fetch("/api/v1/admin/login", { method: "POST", headers: { "Content-Type": "application/json", }, body: JSON.stringify({ username: username, password: password, }), })).json()).token }`, }, })).json()
-	const data = getData('php', 'sucks').then(
-		function (value) { setTableData(value) },
-		function (error) { setTableData(error) },
-	)
-	console.log(tableData)
-	return (<JsonToTable json={tableData}/>)
-}
+	useEffect(() => {
+		getData('php', 'sucks').then(
+			function (value) { setTableData(value) },
+			function (error) { setTableData(error) },
+		)
+	}, [])
+		return (<JsonToTable json={tableData}/>)
+	}
